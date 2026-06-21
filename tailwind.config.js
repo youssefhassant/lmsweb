03940279b028a20d4e55288c8fb2155431f4988c
youssefhassant <133525038+youssefhassant@users.noreply.github.com>
2026-06-22 01:10:17 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+        './node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+        './node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms, require('flowbite/plugin')],
+};
